639dc444afcd7dd06a95335d34feab6ff0b93a38
Dan W <daniel.winn@student.awesomeinc.org>
2022-09-16 04:18:59 +0000

    I broke a lot of things

--- main.js
+++ main.js
@@ -1,32 +1,5 @@
-const currentTime = document.queryselector("display")
+
 const selectMenu = document.querySelectorAll("select");
-
-setInterval(() => {
-    let date = new Date(),
-    h = date.getHours(),
-    m = date.getMinutes(),
-    s = date.getSeconds(),
-    AMPM = "AM";
-
-    if (h>12) {
-        h = h -12;
-        AMPM = "PM";
-    }
-
-    h = h == 0 ? h = 12 : h;
-
-    h = h < 10 ? "0" + h: h;
-    m = m < 10 ? "0" + m: m;
-    s = s < 10 ? "0" + s: s;
-
-    currentTime.insertText = `${h}: ${m}: ${s}: ${AMPM}`;
-}, 1000)
-
-
-
-
-
-
 
 for (let i=12; i>0; i--) {
     i = i < 10 ? "0" + i : i;
@@ -45,3 +18,10 @@ for (let i=2; i > 0; i--) {
     let option = `<option value= "${AMPM}">${AMPM}</option>`;
     selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
 }
+
+let currentTime = newDate()
+
+let time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+
+
+
