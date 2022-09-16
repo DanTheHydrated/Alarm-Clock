f4a842a68069afe0592e64ca3ef1a820be5c58c1
Dan W <daniel.winn@student.awesomeinc.org>
2022-09-16 15:03:01 +0000

    I got the clock to work correctly now I need to transfer my alarm over

--- main.js
+++ main.js
@@ -1,27 +1,39 @@
-
+var time = document.querySelector(".time");
+var dateTime = document.querySelector(".date-time");
 const selectMenu = document.querySelectorAll("select");
 
-for (let i=12; i>0; i--) {
-    i = i < 10 ? "0" + i : i;
-    let option = `<option value= "${i}">${i}</option>`;
-    selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
+
+
+function updateClock() {
+  // Get the current time
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+
+ 
+  // format date and time
+  hours = hours % 12 || 12;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+  date = date < 10 ? "0" + date : date;
+
+  // display time
+  var period = hours < 12 ? "AM" : "PM";
+  time.innerHTML = hours + ":" + minutes + ":" + seconds + " " + period;
+  
 }
+updateClock();
+setInterval(updateClock, 1000);
 
-for (let i=59; i>0; i--) {
-    i = i < 10 ? "0" + i : i;
-    let option = `<option value= "${i}">${i}</option>`;
-    selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option);
-}
 
-for (let i=2; i > 0; i--) {
-    let AMPM = i == 1 ? "AM" : "PM";
-    let option = `<option value= "${AMPM}">${AMPM}</option>`;
-    selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
-}
 
-let currentTime = newDate()
 
-let time = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+
+
+
+
+
 
 
 
